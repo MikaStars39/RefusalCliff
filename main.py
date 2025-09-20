935c0d032569b17cd503a486297742f18a53e2c6
MikaStars39 <meloxdmy@qq.com>
2025-09-20 07:13:59 +0000

    [feat] new fig

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ with open("outputs/train/llama-3-8b-distill/wildjailbreak.json", "r") as f:
     data = json.load(f)
 new_data = []
 
+from src.inference.refusal import refusal_words
+
 for idx in range(len(data)):
+    if any(word.lower() in data[idx]["original_item"]["response"][:128].lower() for word in refusal_words):
+        continue
     new_data.append(
         {
             "instruction": data[idx]["original_item"]["prompt"],
@@ -17,6 +21,6 @@ for idx in range(len(data)):
 import random
 random.shuffle(new_data)
 
-with open("data/wildjailbreak_all.json", "w") as f:
+with open("data/wildjailbreak_rule.json", "w") as f:
     json.dump(new_data, f, indent=4)
 
